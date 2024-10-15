65fb9d1ad69e8842e02e8e5af8e70bc3423fd0a3
sqwizXhar <sonagalkina127@gmail.com>
2024-10-15 15:24:49 +0300

    change group by

--- sortPeopleBySum.sql
+++ sortPeopleBySum.sql
@@ -1,6 +1,6 @@
 SELECT c.name, c.surname, SUM(p.count_of_purchases * pr.price) AS sum
 FROM customers c
-JOIN purchases p ON c.customer_id = p.customer_id
-JOIN products pr ON p.product_id = pr.product_id
-GROUP BY c.customer_id, c.name, c.surname
+JOIN purchases p ON c.id = p.customer_id
+JOIN products pr ON p.product_id = pr.id
+GROUP BY c.id
 ORDER BY sum DESC;
