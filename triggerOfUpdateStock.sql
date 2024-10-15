bc6a2a1b41da0627d3fab3e8fee5512941026711
sqwizXhar <sonagalkina127@gmail.com>
2024-10-15 15:28:00 +0300

    update trigger

--- triggerOfUpdateStock.sql
+++ triggerOfUpdateStock.sql
@@ -1,18 +1,17 @@
-CREATE OR REPLACE FUNCTION update_stock() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_and_update_stock() 
+RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.count_of_purchases > (SELECT stock FROM products WHERE product_id = NEW.product_id) THEN
+    IF NEW.count_of_purchases > (SELECT stock FROM products WHERE id = NEW.product_id FOR UPDATE) THEN
         RAISE EXCEPTION 'Недостаточно товара на складе';
     END IF;
-
     UPDATE products
     SET stock = stock - NEW.count_of_purchases
-    WHERE product_id = NEW.product_id;
-
+    WHERE id = NEW.product_id;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_stock_trigger
-AFTER INSERT ON purchases
+CREATE TRIGGER check_and_update_stock_trigger
+BEFORE INSERT ON purchases
 FOR EACH ROW
-EXECUTE FUNCTION update_stock();
+EXECUTE FUNCTION check_and_update_stock();
